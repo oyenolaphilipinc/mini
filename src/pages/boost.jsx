ad43d8561453b45c0b8bf9acb5734daf7624eebc
Oyenola Philip <76276196+oyenolaphilipinc@users.noreply.github.com>
2024-06-25 23:32:04 -0700

    Done the api for the boost page
    Done

--- src/pages/boost.jsx
+++ src/pages/boost.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoMdClose } from "react-icons/io";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { Poppins } from "next/font/google";
 import Image from 'next/image';
+import { useRouter } from 'next/router';
   
   const poppins = Poppins({
     subsets: ["latin"],
@@ -22,6 +23,24 @@ import Image from 'next/image';
   });
 
 const Boost = () => {
+    const router = useRouter();
+    const { userId } = router.query;
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        const fetchBalance = async() => {
+          try {
+            const res = await axios.get(`/api/getTapDetailsByUserId`, { userId });
+            if (res.data.success) {
+              setCount(res.data.data.tapBalance);
+            }
+          } catch (error) {
+            console.error('Error fetching balance:', error);
+          }
+        }
+    
+        fetchBalance();
+      }, [userId])
   return (
     <div className={`bg-[#1d1d1d] h-screen ${poppins.className} text-white`}>
         <div className="mb-8 pt-8 border-b rounded-md pb-4 border-[#fbce47]">
@@ -34,7 +53,7 @@ const Boost = () => {
         </div>
         </div>
         <div className="coin border flex justify-between text-white border-[#1d1d1d] bg-[#282828] w-11/12 mx-auto px-2 py-2 mt-4 rounded-md">
-            <h1 className="flex pl-4 text-4xl font-bold"><Image src={"/coin.svg"} height={40} width={40} className="mr-1" /> 6122</h1>
+            <h1 className="flex pl-4 text-4xl font-bold"><Image src={"/coin.svg"} height={40} width={40} className="mr-1" />{count}</h1>
             <div className="pr-6">
                 <p className="text-sm font-normal">Level</p>
                 <p className="text-sm font-semibold">1</p>
